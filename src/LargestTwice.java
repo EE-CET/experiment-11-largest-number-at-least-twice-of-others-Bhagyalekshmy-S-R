7aad82fa8b85f903ce0481fb3a5781616142a87f
Your Name <you@example.com>
2026-01-15 12:30:49 +0530

    first commit

--- src/LargestTwice.java
+++ src/LargestTwice.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 public class LargestTwice {
     
-        // TODO: Read n
-        // TODO: Read array elements
-        // TODO: Find the largest element and its index
-        // TODO: Check if largest >= 2 * every other element
-        // TODO: Print index or -1
+        public static int findTwice(int[] arr,int n){
+                 if(n==0)
+                    return -1;
+                 int largest=0;
+                 for(int i=1;i<n;i++){
+                    if(arr[i]>=arr[largest]){
+                          largest=i;
+                          }
+                       }
+                 for(int i=0;i<n;i++){ 
+                     if(i != largest && arr[largest] < 2 * arr[i]){
+                            return -1;
+                          }
+                       }
+                  return largest;
+              }
+         public static void main(String[] args){
+              Scanner sc = new Scanner(System.in);
+              int n=sc.nextInt();
+              int[] arr= new int[n];
+              for(int i=0;i<n;i++){
+                  arr[i]=sc.nextInt();
+                  }
+              System.out.println(findTwice(arr,n));
+        }              
     
 }
